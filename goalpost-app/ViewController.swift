a359e27a86af8c5cb870fb6ffeea87c5447ab753
Manoli <igielskv@icloud.com>
2020-04-15 07:30:30 +0200

    Initial Commit

---
+++ goalpost-app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  goalpost-app
+//
+//  Created by Manoli on 15/04/2020.
+//  Copyright © 2020 Manoli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
